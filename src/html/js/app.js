1247f23b6ad654038a0d8399499fbd598f94e46a
Roshan <roshanjoshi806089@gmail.com>
2016-04-06 14:06:12 +0530

    Mouseover mouseout events on hovered path area

--- src/html/js/app.js
+++ src/html/js/app.js
@@ -5,7 +5,7 @@
     height = 500;
 
   var path = d3.geo.path();
-
+  var pathVar;
   var svg = d3.select('.chart').append('svg')
     .attr('width', width)
     .attr('height', height);
@@ -13,10 +13,28 @@
   d3.json('data/us.json', function(error, us) {
     if (error) throw error;
 
-    svg.selectAll('path')
+    pathVar = svg.selectAll('path')
       .data(topojson.feature(us, us.objects.states).features)
-      .enter().append('path')
-      .attr('d', path);
+      .enter().append('path');
+      pathVar.attr('d', path)
+      .attr('class','abc');
+      pathVar.on("mouseover", function() {
+          expandAreaPath(this);
+       });
+      pathVar.on("mouseout", function() {
+          compressAreaPath(this);
+       });
     
   });
+
+function expandAreaPath(pathElem) {
+  d3.select(pathElem).style('fill','red');
+  d3.select(pathElem).style({'stroke':'black','stroke-width':'1px'})
+}
+
+function compressAreaPath(pathElem){
+  d3.select(pathElem).style('fill','#ccc');
+  d3.select(pathElem).style({'stroke':'#fff','stroke-width':'.5px'})
+}
+
 })();
