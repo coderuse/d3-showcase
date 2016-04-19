09f811b9a4c3203c74e9e7443c9990702322c24d
Roshan <roshanjoshi806089@gmail.com>
2016-04-19 12:43:39 +0530

    Converted code to oops

--- src/html/js/app.js
+++ src/html/js/app.js
@@ -1,95 +1,6 @@
 (function(w) {
   'use strict';
-
-  // Declaring lib variables
-  var d3 = w.d3,
-    topojson = w.topojson;
-
-  var width = 800,
-    height = 500,
-    centered;
-
-  var projection = d3.geo.albersUsa()
-    .scale(800)
-    .translate([width / 2, height / 2]);
-
-  var path = d3.geo.path().projection(projection);
-  var pathVar = [];
-  var svg = d3.select('.map').append('svg')
-    .attr('width', width)
-    .attr('height', height);
-
-  var g = svg.append("g");
-
-  var tooltip = d3.select('body').append('div')
-    .style({
-      position: 'absolute',
-      padding: '5px',
-      color: '#fff',
-      background: '#000',
-      'border-radius': '5px',
-      opacity: 0,
-      'font-size': 'medium'
-    });
-    
-  function colorGenerator(minValue,maxValue,baseColor,step){
-      var mean = d3.mean([minValue,maxValue]);
-      var color = d3.hsl(baseColor);
-      return function(value){
-        return color.darker(step*(value-mean)/mean).toString();
-      };
-  }
-  
-  var chartWidth = 500;
-  var chartHeight = 200;
-  var padding = 20;
-  var chartSvg = d3.select('.charts').append('svg')
-      .attr('width', chartWidth)
-      .attr('height', chartHeight);
-      
-  var barChart = chartSvg.append('g')
-    .attr('class','bar-chart');
-  
-  function drawBarChart(config){
-    var values = config.data.map(function(datum){return datum[config.property];});
-    var barScale = d3.scale.ordinal()
-      .domain(config.data.map(function(datum){return datum.id;}))
-      .rangeBands([0,chartWidth],0.3);
-    var barWidth = barScale.rangeBand();
-    var valueScale = d3.scale.linear()
-      .domain([d3.min(values)-1000,d3.max(values)+1000])
-      .range([chartHeight-padding,0]);
-    barChart.selectAll('g.bar-axis').remove();
-    if(config.showAxis){
-      var barAxis = d3.svg.axis().scale(barScale).tickFormat(function(id){
-        return config.data.find(function(datum){return datum.id == id;}).name;
-      });
-      barChart.append('g')
-        .attr('class','bar-axis')
-        .attr('transform', 'translate(0,'+(chartHeight-padding)+')')
-        .call(barAxis);
-    }
-    var barSet = barChart.selectAll('rect.bar').data(config.data,function(datum){return datum.id;});
-    barSet.exit().remove();
-    barSet.enter().append('rect').attr('class','bar');
-    barSet
-      .attr('x',function(datum){return barScale(datum.id);})
-      .attr('y',chartHeight-padding)
-      .attr('width',barWidth)
-      .attr('height',0)
-      .attr('fill','red')
-      .on('click',config.onBarClick);
-    barSet.append('title')
-        .text(function(datum){return datum.name;});
-    barSet
-      .transition().duration(1000).ease('ease-in')
-      .attr('height',function(datum){return chartHeight - padding - valueScale(datum[config.property]);})
-      .attr('y',function(datum){return valueScale(datum[config.property]);})
-      .attr('fill',config.colorScale);
-  }
-
-
-  d3.json('data/us.json', function(error, us) {
+d3.json('data/us.json', function(error, us) {
 
     if (error) {
       throw error;
@@ -97,10 +8,56 @@
     var data = topojson.feature(us, us.objects.states).features;
 
     d3.tsv('data/us-state-names.tsv', function(stateMapping) {
+      drawCharts(data,stateMapping);
+});
+});
 
-      var states = [];
-      var maxPopulationValue, minPopulationValue, maxUnInsuredValue, minUnInsuredValue;
-      stateMapping.forEach(function(state) {
+function drawCharts(data,stateMapping){
+  var obj = {
+  width:800,
+  height:500,
+  svgxPos:0,
+  svgyPos:0,
+  data:data,
+  stateMapping:stateMapping,
+  barChartSvgWidth:500,
+  barChartSvgHeight:200,
+}
+      var usChart = new usMapChart(obj);
+      var elem = usChart.initialize('.map',obj.width,obj.height);
+      usChart.createMap(elem,data);
+}
+
+
+var usMapChart = (function() {
+    function usMapChart(_obj){
+      var self = this;
+      self.width = _obj.width;
+      self.height = _obj.height;
+      self.svgxPos =_obj.svgxPos;
+      self.svgyPos = _obj.svgyPos;
+      self.padding = 20;
+      self.barChartSvgWidth = _obj.barChartSvgWidth;
+      self.barChartSvgHeight = _obj.barChartSvgHeight;
+      self.centered;
+      self.projection = d3.geo.albersUsa()
+                        .scale(800)
+                        .translate([this.width / 2, this.height / 2]);
+      self.path = d3.geo.path().projection(this.projection);
+      self.tooltip = d3.select('body').append('div')
+                    .style({
+                    position: 'absolute',
+                    padding: '5px',
+                    color: '#fff',
+                    background: '#000',
+                    'border-radius': '5px',
+                    opacity: 0,
+                    'font-size': 'medium'
+                  });
+       
+       var states =[];
+      self.maxPopulationValue, self.minPopulationValue, self.maxUnInsuredValue, self.minUnInsuredValue;
+      _obj.stateMapping.forEach(function(state) {
         states.push({
           id:parseInt(state.id),
           name:state.name,
@@ -109,167 +66,242 @@
           unInsured:parseInt(state.unInsured)
         });
       });
-      
-      var getState = function(id){
-        return states.find(function(state){return state.id === id;});
-      };
-      
-      var getComparableStates = function(id,property){
-        var max, min, h1, h2, h3, l1, l2, l3, maxSet, minSet;
-        var outValues = [];
-        maxSet = states
-          .filter(function(state){return state[property]!==0 && (state[property] > getState(id)[property]);})
-          .sort(function(state1,state2){return state1[property] - state2[property];});
-        minSet = states
-          .filter(function(state){return state[property]!==0 && (state[property] < getState(id)[property]);})
-          .sort(function(state1,state2){return state2[property] - state1[property];});
-        if(maxSet[2]){outValues.push(maxSet[2]);}
-        if(maxSet[1]){outValues.push(maxSet[1]);}
-        if(maxSet[0]){outValues.push(maxSet[0]);}
-        outValues.push(getState(id));
-        if(minSet[0]){outValues.push(minSet[0]);}
-        if(minSet[1]){outValues.push(minSet[1]);}
-        if(minSet[2]){outValues.push(minSet[2]);}
-        return outValues;
-      };
-
-      maxPopulationValue = d3.max(
-        states
+     self.states = states;
+     
+      self.maxPopulationValue = d3.max(
+        self.states
           .map(function(state){return state.population;})
           .filter(function(value){return value !== 0;})
       );
 
-      minPopulationValue = d3.min(
-        states
+      self.minPopulationValue = d3.min(
+        self.states
           .map(function(state){return state.population;})
           .filter(function(value){return value !== 0;})
       );
-      
-      maxUnInsuredValue = d3.max(
-        states
+
+       self.maxUnInsuredValue = d3.max(
+        self.states
           .map(function(state){return state.unInsured;})
           .filter(function(value){return value!==0;})
       );
       
-      minUnInsuredValue = d3.min(
-        states
+      self.minUnInsuredValue = d3.min(
+        self.states
           .map(function(state){return state.unInsured;})
           .filter(function(value){return value !==0;})
       );
       
-      var blueShadesForPopulation = colorGenerator(minPopulationValue,maxPopulationValue,'blue',1.75);
-      var redShadesForUnInsured = colorGenerator(minUnInsuredValue,maxUnInsuredValue,'red',1.75);
+      self.blueShadesForState = self.colorGenerator(self.minPopulationValue,self.maxPopulationValue,'blue',1.75); 
+      self.redShadesForUnInsured = self.colorGenerator(self.minUnInsuredValue,self.maxUnInsuredValue,'red',1.75);
       
-      var colorScale = function(state){
-        return redShadesForUnInsured(state.unInsured);
+      //console.log(this.redShadesForUnInsured)
+      self.colorScale = function(state){
+        return self.redShadesForUnInsured(state.unInsured);
       };
-      
-      var onBarClick = function(state){
-        goThroughState(data.find(function(datum){return datum.id==state.id;}));
+
+      self.onBarClick = function(state){
+        self.goThroughState(_obj.data.find(function(datum){return datum.id==state.id;}));
         d3.event.stopPropagation();
       };
-      
-      drawBarChart({
-        data:states.filter(function(state){return state.income!==0;}),
-        property:'income',
-        colorScale:colorScale,
-        onBarClick:onBarClick
-      });
 
-      var gEl = g.selectAll('.state')
+      self.svgForBarChart = self.initialize('.charts',self.barChartSvgWidth,self.barChartSvgHeight);
+
+      self.drawBarChart(self.svgForBarChart,{data:self.states
+          .filter(function(state){return state.income!==0;}),
+          property:'income',
+          colorScale:self.colorScale,
+        onBarClick:self.onBarClick});
+          
+
+    };
+       usMapChart.prototype.initialize = function (elem,width,height) {
+            this.svg = d3.select(elem).append('svg')
+            .attr('width',width)
+            .attr('height', height);
+            this.g = this.svg.append("g");
+            return this.g;
+      };
+    usMapChart.prototype.colorGenerator = function(minValue,maxValue,baseColor,step){
+      var mean = d3.mean([minValue,maxValue]);
+      var color = d3.hsl(baseColor);
+      return function(value){
+        return color.darker(step*(value-mean)/mean).toString();
+      };
+    };
+    
+    usMapChart.prototype.getState = function(id){
+        return this.states.filter(function(state){return state.id === id;})[0];
+      };
+
+  usMapChart.prototype.goThroughState = function(d) {
+    var x, y, k, centered = this.centered;
+
+    if (d && this.centered !== d) {
+      var centroid = this.path.centroid(d);
+      x = centroid[0];
+      y = centroid[1];
+      k = 3;
+      this.centered = d;
+      this.transformStates(x, y, k,d, false);
+      this.g.selectAll('path:not(.active)')
+        .transition()
+        .duration(1000)
+        .style('opacity', '0.5');
+    } else {
+      x = this.width / 2;
+      y = this.height / 2;
+      k = 1;
+      this.centered = null;
+      this.g.selectAll('path')
+        .transition()
+        .duration(1000)
+        .style('opacity', '1');
+      this.transformStates(x, y, k);
+    }
+
+  };
+
+  usMapChart.prototype.transformStates = function(x, y, k,d, state) {
+    var centered = this.centered;
+    // console.log(d);
+
+    this.g.selectAll("path")
+     .classed("active", centered && function(d) {
+        return d === centered;
+      });
+    this.g.transition()
+      .duration(1000)
+      .attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+      .style("stroke-width", 1.5 / k + "px");
+
+    if (state === true) {
+      d3.select('path.active').classed("active", false);
+      this.g.selectAll('path')
+        .transition()
+        .duration(1000)
+        .style('opacity', '1');
+    }
+
+  }
+
+usMapChart.prototype.getComparableStates = function(id,property){
+        var max, min, h1, h2, h3, l1, l2, l3, maxSet, minSet, self=this;
+        var outValues = [];
+        var values = this.states
+          .map(function(state){return state[property];})
+          .filter(function(value){return value!==0;});
+          // .sort(function(value1,value2){return value2-value1;});
+        max = self.states.filter(function(state){return state[property] === d3.max(values);})[0];
+        min = self.states.filter(function(state){return state[property] === d3.min(values);})[0];
+        maxSet = self.states
+          .filter(function(state){return state[property]!==0 && (state[property] > self.getState(id)[property]);})
+          .sort(function(state1,state2){return state1[property] - state2[property];});
+        minSet = self.states
+          .filter(function(state){return state[property]!==0 && (state[property] < self.getState(id)[property]);})
+          .sort(function(state1,state2){return state2[property] - state1[property];});
+        outValues.push(min);
+        if(minSet[2] && (minSet[2].id!==min.id)){outValues.push(minSet[2]);}
+        if(minSet[1] && (minSet[1].id!==min.id)){outValues.push(minSet[1]);}
+        if(minSet[0] && (minSet[0].id!==min.id)){outValues.push(minSet[0]);}
+        if(id !== min.id && id !== max.id){outValues.push(self.getState(id));}
+        if(maxSet[0] && (maxSet[0].id!==max.id)){outValues.push(maxSet[0]);}
+        if(maxSet[1] && (maxSet[1].id!==max.id)){outValues.push(maxSet[1]);}
+        if(maxSet[2] && (maxSet[2].id!==max.id)){outValues.push(maxSet[2]);}
+        outValues.push(max);
+        return outValues;
+      };
+
+
+    usMapChart.prototype.createMap = function (groupElem,data) {
+      var self=this;
+       var gEl = groupElem.selectAll('.state')
         .data(data)
         .enter().append('g')
         .attr('class', 'state');
 
       gEl.append('path')
-        .attr('d', path)
+        .attr('d', this.path)
         .attr('fill',function(d){
-          if(getState(d.id).population === 0){
+          if(self.getState(d.id).population === 0){
             return 'red';
           } else {
-            return blueShadesForPopulation(getState(d.id).population);
+            return self.blueShadesForState(self.getState(d.id).population);
           }
         })
         .on("click", function(d) {
           if ((d3.select(this).style('opacity')) != 1) {
-            transformStates(width / 2, height / 2, 1, true);
-            drawBarChart({
-              data:states.filter(function(state){return state.income!==0;}),
+            self.transformStates(self.width / 2, self.height / 2, 1,d, true);
+             self.drawBarChart(self.svgForBarChart,{
+              data:self.states.filter(function(state){return state.income!==0;}),
               property:'income',
-              colorScale:colorScale,
-              onBarClick:onBarClick
+              colorScale:self.colorScale,
+              onBarClick:self.onBarClick
             });
           } else {
-            goThroughState(d);
-            drawBarChart({
-              data:getComparableStates(d.id,'income'),
-              property:'income',
-              colorScale:colorScale,
-              onBarClick:onBarClick,
-              showAxis:true
-            });
+            self.goThroughState(d);
+             self.drawBarChart(self.svgForBarChart,{
+            data:self.getComparableStates(d.id,'income'),
+            property:'income',
+            colorScale:self.colorScale,
+              onBarClick:self.onBarClick
+          });
           }
         })
         .on('mouseover', function(d) {
-          tooltip.transition().style('opacity', 0.6);
-          tooltip.html(getState(d.id).name)
+
+          self.tooltip.transition().style('opacity', 0.6);
+          self.tooltip.html(self.getState(d.id).name)
             .style({
               'left': (d3.event.pageX + 5) + 'px',
               'top': (d3.event.pageY - 20) + 'px'
             });
         })
         .on('mouseout', function() {
-          tooltip.transition().style('opacity', 0);
+          self.tooltip.transition().style('opacity', 0);
         });
-    });
-  });
-
-  function goThroughState(d) {
-    var x, y, k;
-
-    if (d && centered !== d) {
-      var centroid = path.centroid(d);
-      x = centroid[0];
-      y = centroid[1];
-      k = 3;
-      centered = d;
-      transformStates(x, y, k, false);
-      g.selectAll('path:not(.active)')
-        .transition()
-        .duration(1000)
-        .style('opacity', '0.5');
-    } else {
-      x = width / 2;
-      y = height / 2;
-      k = 1;
-      centered = null;
-      g.selectAll('path')
-        .transition()
-        .duration(1000)
-        .style('opacity', '1');
-      transformStates(x, y, k);
-    }
-
-  }
-
-  function transformStates(x, y, k, state) {
-    g.selectAll("path")
-      .classed("active", centered && function(d) {
-        return d === centered;
+    };
+usMapChart.prototype.drawBarChart = function(barChartElem,config){
+  var self=this;
+    var values = config.data.map(function(datum){return datum[config.property];});
+    var barScale = d3.scale.ordinal()
+      .domain(config.data.map(function(datum){return datum.id;}))
+      .rangeBands([0,this.barChartSvgWidth],0.3);
+    var barWidth = barScale.rangeBand();
+    var valueScale = d3.scale.linear()
+      .domain([d3.min(values)-1000,d3.max(values)+1000])
+      .range([this.barChartSvgHeight-this.padding,0]);
+       if(config.showAxis){
+      var barAxis = d3.svg.axis().scale(barScale).tickFormat(function(id){
+        return config.data.find(function(datum){return datum.id == id;}).name;
       });
-
-    g.transition()
-      .duration(1000)
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-      .style("stroke-width", 1.5 / k + "px");
-
-    if (state === true) {
-      d3.select('path.active').classed("active", false);
-      g.selectAll('path')
-        .transition()
-        .duration(1000)
-        .style('opacity', '1');
+      barChartElem
+      .attr('class','bar-chart')
+      .append('g')
+        .attr('class','bar-axis')
+        .attr('transform', 'translate(0,'+(self.barChartSvgHeight-self.padding)+')')
+        .call(barAxis);
     }
-
+   var barSet = barChartElem.selectAll('rect.bar').data(config.data,function(datum){return datum.id;});
+    barSet.exit().remove();
+    barSet.enter().append('rect').attr('class','bar');
+    barSet
+      .attr('x',function(datum){return barScale(datum.id);})
+      .attr('y',(self.barChartSvgHeight-self.padding))
+      .attr('width',barWidth)
+      .attr('height',0)
+      .attr('fill','red')
+      .on('click',config.onBarClick);
+    barSet.append('title')
+        .text(function(datum){return datum.name;});
+    barSet
+      .transition().duration(1000).ease('ease-in')
+      .attr('height',function(datum){return (self.barChartSvgHeight-self.padding) - valueScale(datum[config.property]);})
+      .attr('y',function(datum){return valueScale(datum[config.property]);})
+      .attr('fill',config.colorScale);
   }
+
+
+  return usMapChart;
+})()
 })(window);
